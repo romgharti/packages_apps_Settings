645cd2d002bcce0dd75bd93fd04bc39116c8d0fe
Jackeagle <jackeagle102@gmail.com>
2017-05-04 13:36:21 -0400

    Settings: Integrate the dumpster into Settings
    - Adapt to Android R Changes
    - Keep it below Connected Devices
    
    Change-Id: I30a65c3e17aff251eef6bbf2454203bbed3300ae
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: rohan <purohit.rohan@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -516,4 +516,9 @@ public class Settings extends SettingsActivity {
      * Activity for OneHandedSettings
      */
     public static class OneHandedSettingsActivity extends SettingsActivity { /* empty */ }
+
+    /**
+     * Bootleggers custom activities
+     */
+    public static class BootlegDumpsterActivity extends SettingsActivity {}
 }
